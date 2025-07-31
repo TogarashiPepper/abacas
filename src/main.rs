18e0783ee6bdede0b62caae93f32da64a3940446
TogarashiPepper <83902606+TogarashiPepper@users.noreply.github.com>
2025-07-30 20:34:33 -0700

    feat: lexer

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use lexer::lex;
+
+mod lexer;
+
 fn main() {
-    println!("Hello, world!");
+    let st = "1 + 23 * 234 / 2345 ^ 10";
+    let tks = lex(st).unwrap();
+
+    println!("{tks:?}")
 }
