0eaaa6c75967b17ad63ae4017f0cc81b69bcc67f
TogarashiPepper <83902606+TogarashiPepper@users.noreply.github.com>
2025-07-31 19:25:59 -0700

    feat: simple parser

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
-use lexer::lex;
-
 mod lexer;
+mod parser;
+
+use lexer::lex;
+use parser::expr_bp;
 
 fn main() {
     let st = "1 + 23 * 234 / 2345 ^ 10";
     let tks = lex(st).unwrap();
+    let ast = expr_bp(&mut tks.into_iter().peekable(), 0).unwrap();
 
-    println!("{tks:?}")
+    println!("{ast:#?}");
 }
