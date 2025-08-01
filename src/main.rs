b474a3f8760b0ab919dec682c5459f0023c20165
TogarashiPepper <83902606+TogarashiPepper@users.noreply.github.com>
2025-08-01 13:34:32 -0700

    feat: polynomial + polynomial support

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 mod lexer;
-mod parser;
 mod monomial;
+mod parser;
 mod polynomial;
 
 use lexer::lex;
@@ -13,10 +13,11 @@ fn main() {
     // let ast = expr_bp(&mut tks.into_iter().peekable(), 0).unwrap();
 
     // println!("{ast:#?}");
-    
+
     let m = Monomial::new(4.0, 2);
     let r = Monomial::new(5.0, 2);
     let res = m + r + Monomial::new(1.0, 1000);
+    let res = res + (Monomial::new(10.0, 10) + Monomial::new(9.0, 9));
 
-    println!("{res:?}"); // prints [x^1000, 9x^2]
+    println!("{res:?}"); // prints [x^1000, 10x^10, 9x^9, 9x^2]
 }
