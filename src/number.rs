05b3eb2ed4452d15de70dac31c3d9382f7740190
megatank58 <51410502+megatank58@users.noreply.github.com>
2025-10-16 19:31:37 +0530

    feat(Number): replace wrapper structs with rug types in enum

--- src/number.rs
+++ src/number.rs
@@ -1,21 +1,21 @@
 use std::ops::{Add, Div, Mul, Sub};
 
-use rug::{Integer as RugInteger, Rational as RugRational};
+use rug::{Integer, Rational};
 
 #[derive(Clone, Debug)]
 pub enum Number {
-	Natural(Natural),
+	Natural(Integer),
 	Integer(Integer),
-	Real(Real),
+	Real(Rational),
 }
 
 impl Number {
-	pub fn from_rug_integer(n: RugInteger) -> Self {
-		Self::Integer(Integer(n))
+	pub fn from_rug_integer(n: Integer) -> Self {
+		Self::Integer(n)
 	}
 
-	pub fn from_rug_rational(n: RugRational) -> Self {
-		Self::Real(Real(n))
+	pub fn from_rug_rational(n: Rational) -> Self {
+		Self::Real(n)
 	}
 }
 
@@ -24,18 +24,18 @@ impl Add for Number {
 
 	fn add(self, rhs: Self) -> Self::Output {
 		match (self, rhs) {
-			(Number::Natural(n1), Number::Natural(n2)) => Number::Natural(Natural(n1.0 + n2.0)),
+			(Number::Natural(n1), Number::Natural(n2)) => Number::Natural(n1 + n2),
 			(Number::Natural(n), Number::Integer(i)) | (Number::Integer(i), Number::Natural(n)) => {
-				Number::from_rug_integer(n.0 + i.0)
+				Number::from_rug_integer(n + i)
 			}
 			(Number::Natural(n), Number::Real(r)) | (Number::Real(r), Number::Natural(n)) => {
-				Number::from_rug_rational(n.0 + r.0)
+				Number::from_rug_rational(n + r)
 			}
-			(Number::Integer(i1), Number::Integer(i2)) => Number::from_rug_integer(i1.0 + i2.0),
+			(Number::Integer(i1), Number::Integer(i2)) => Number::from_rug_integer(i1 + i2),
 			(Number::Integer(i), Number::Real(r)) | (Number::Real(r), Number::Integer(i)) => {
-				Number::from_rug_rational(i.0 + r.0)
+				Number::from_rug_rational(i + r)
 			}
-			(Number::Real(r1), Number::Real(r2)) => Number::from_rug_rational(r1.0 + r2.0),
+			(Number::Real(r1), Number::Real(r2)) => Number::from_rug_rational(r1 + r2),
 		}
 	}
 }
@@ -45,18 +45,18 @@ impl Sub for Number {
 
 	fn sub(self, rhs: Self) -> Self::Output {
 		match (self, rhs) {
-			(Number::Natural(n1), Number::Natural(n2)) => Number::Natural(Natural(n1.0 - n2.0)),
+			(Number::Natural(n1), Number::Natural(n2)) => Number::Natural(n1 - n2),
 			(Number::Natural(n), Number::Integer(i)) | (Number::Integer(i), Number::Natural(n)) => {
-				Number::from_rug_integer(n.0 - i.0)
+				Number::from_rug_integer(n - i)
 			}
 			(Number::Natural(n), Number::Real(r)) | (Number::Real(r), Number::Natural(n)) => {
-				Number::from_rug_rational(n.0 - r.0)
+				Number::from_rug_rational(n - r)
 			}
-			(Number::Integer(i1), Number::Integer(i2)) => Number::from_rug_integer(i1.0 - i2.0),
+			(Number::Integer(i1), Number::Integer(i2)) => Number::from_rug_integer(i1 - i2),
 			(Number::Integer(i), Number::Real(r)) | (Number::Real(r), Number::Integer(i)) => {
-				Number::from_rug_rational(i.0 - r.0)
+				Number::from_rug_rational(i - r)
 			}
-			(Number::Real(r1), Number::Real(r2)) => Number::from_rug_rational(r1.0 - r2.0),
+			(Number::Real(r1), Number::Real(r2)) => Number::from_rug_rational(r1 - r2),
 		}
 	}
 }
@@ -66,18 +66,18 @@ impl Mul for Number {
 
 	fn mul(self, rhs: Self) -> Self::Output {
 		match (self, rhs) {
-			(Number::Natural(n1), Number::Natural(n2)) => Number::Natural(Natural(n1.0 * n2.0)),
+			(Number::Natural(n1), Number::Natural(n2)) => Number::Natural(n1 * n2),
 			(Number::Natural(n), Number::Integer(i)) | (Number::Integer(i), Number::Natural(n)) => {
-				Number::from_rug_integer(n.0 * i.0)
+				Number::from_rug_integer(n * i)
 			}
 			(Number::Natural(n), Number::Real(r)) | (Number::Real(r), Number::Natural(n)) => {
-				Number::from_rug_rational(n.0 * r.0)
+				Number::from_rug_rational(n * r)
 			}
-			(Number::Integer(i1), Number::Integer(i2)) => Number::from_rug_integer(i1.0 * i2.0),
+			(Number::Integer(i1), Number::Integer(i2)) => Number::from_rug_integer(i1 * i2),
 			(Number::Integer(i), Number::Real(r)) | (Number::Real(r), Number::Integer(i)) => {
-				Number::from_rug_rational(i.0 * r.0)
+				Number::from_rug_rational(i * r)
 			}
-			(Number::Real(r1), Number::Real(r2)) => Number::from_rug_rational(r1.0 * r2.0),
+			(Number::Real(r1), Number::Real(r2)) => Number::from_rug_rational(r1 * r2),
 		}
 	}
 }
@@ -87,45 +87,18 @@ impl Div for Number {
 
 	fn div(self, rhs: Self) -> Self::Output {
 		match (self, rhs) {
-			(Number::Natural(n1), Number::Natural(n2)) => Number::Natural(Natural(n1.0 / n2.0)),
+			(Number::Natural(n1), Number::Natural(n2)) => Number::Natural(n1 / n2),
 			(Number::Natural(n), Number::Integer(i)) | (Number::Integer(i), Number::Natural(n)) => {
-				Number::from_rug_integer(n.0 / i.0)
+				Number::from_rug_integer(n / i)
 			}
 			(Number::Natural(n), Number::Real(r)) | (Number::Real(r), Number::Natural(n)) => {
-				Number::from_rug_rational(n.0 / r.0)
+				Number::from_rug_rational(n / r)
 			}
-			(Number::Integer(i1), Number::Integer(i2)) => Number::from_rug_integer(i1.0 / i2.0),
+			(Number::Integer(i1), Number::Integer(i2)) => Number::from_rug_integer(i1 / i2),
 			(Number::Integer(i), Number::Real(r)) | (Number::Real(r), Number::Integer(i)) => {
-				Number::from_rug_rational(i.0 / r.0)
+				Number::from_rug_rational(i / r)
 			}
-			(Number::Real(r1), Number::Real(r2)) => Number::from_rug_rational(r1.0 / r2.0),
+			(Number::Real(r1), Number::Real(r2)) => Number::from_rug_rational(r1 / r2),
 		}
-	}
-}
-
-#[derive(Clone, Debug)]
-pub struct Natural(RugInteger);
-
-impl Natural {
-	pub fn new(n: usize) -> Self {
-		Self(RugInteger::from(n))
-	}
-}
-
-#[derive(Clone, Debug)]
-pub struct Integer(RugInteger);
-
-impl Integer {
-	pub fn new(n: isize) -> Self {
-		Self(RugInteger::from(n))
-	}
-}
-
-#[derive(Clone, Debug)]
-pub struct Real(RugRational);
-
-impl Real {
-	pub fn new(n: f64) -> Self {
-		Self(RugRational::from_f64(n).unwrap())
 	}
 }
