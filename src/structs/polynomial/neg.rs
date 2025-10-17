aadf320a9c85118fe4f2e158f9f49f8f7cb2a064
vaporoxx <jan.zichler@gmx.de>
2025-10-17 11:12:47 +0200

    feat: implement neg assign trait

--- src/structs/polynomial/neg.rs
+++ src/structs/polynomial/neg.rs
@@ -1,15 +1,22 @@
 use std::ops::Neg;
 
+use rug::ops::NegAssign;
+
 use crate::structs::Polynomial;
 
 impl Neg for Polynomial {
 	type Output = Polynomial;
 
 	fn neg(mut self) -> Self::Output {
-		for monomial in self.0.iter_mut() {
-			*monomial = -*monomial;
-		}
-
+		self.neg_assign();
 		self
+	}
+}
+
+impl NegAssign for Polynomial {
+	fn neg_assign(&mut self) {
+		for monomial in self.0.iter_mut() {
+			monomial.neg_assign();
+		}
 	}
 }
