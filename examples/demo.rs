42b5f5c7e09d0323ce6d36392f4092ff39a287de
Jan <66554238+vaporoxx@users.noreply.github.com>
2025-10-20 11:29:19 +0200

    feat: add actually useful example (#25)

--- examples/demo.rs
+++ examples/demo.rs
@@ -1,20 +1,20 @@
 use abacas::structs::Polynomial;
 
-const GREEN: &str = "\x1b[38;5;10m";
-const RED: &str = "\x1b[38;5;1m";
-const PURPLE: &str = "\x1b[38;5;33m";
+const GREEN: &str = "\x1b[32m";
+const RED: &str = "\x1b[31m";
 const RESET: &str = "\x1b[0m";
 
 fn main() {
-	let a = "x^3 + 2x^2 + 3x + 4".parse::<Polynomial>().unwrap();
-	let b = "9x^4 + 3x^3 + 2x + 1".parse::<Polynomial>().unwrap();
+	let a: Polynomial = "x^3 + 2x^2 + 3x + 4".parse().unwrap();
+	let b: Polynomial = "9x^4 + 3x^3 + 2x + 1".parse().unwrap();
 
-	let fa = format!("{GREEN}a{RESET}");
-	let fb = format!("{RED}b{RESET}");
+	let fa = format_args!("{GREEN}a{RESET}");
+	let fb = format_args!("{RED}b{RESET}");
 
-	println!("let {fa} = {GREEN}{a}{RESET};");
-	println!("let {fb} = {RED}{b}{RESET};");
-	println!("{fa} + {fb} → {PURPLE}{}{RESET}", a.clone() + b.clone());
-	println!("{fa} - {fb} → {PURPLE}{}{RESET}", a.clone() - b.clone());
-	println!("{fa} * {fb} → {PURPLE}{}{RESET}", a.clone() * b.clone());
+	println!("let {fa} = {GREEN}{a}{RESET}");
+	println!("let {fb} = {RED}{b}{RESET}");
+
+	println!("\n{fa} + {fb} = {}", a.clone() + b.clone());
+	println!("{fa} - {fb} = {}", a.clone() - b.clone());
+	println!("{fa} * {fb} = {}", a.clone() * b.clone());
 }
