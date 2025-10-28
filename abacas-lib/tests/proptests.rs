713c8f735ce456783b7986681a03c37f4f0fecd2
vaporoxx <jan.zichler@gmx.de>
2025-10-28 23:15:57 +0100

    refactor: move proptests to separate file

---
+++ abacas-lib/tests/proptests.rs
@@ -0,0 +1,49 @@
+use abacas::monomial::Monomial;
+use abacas::polynomial::Polynomial;
+
+/// Helper to construct a random polynomial with the given degree.
+fn random_poly(degree: usize) -> Polynomial {
+	let mut poly = Vec::with_capacity(degree + 1);
+
+	for degree in 0..=degree {
+		let numer = rand::random_range(1..1000);
+		let denom = rand::random_range(1..1000);
+
+		poly.push(Monomial::new((numer, denom), degree));
+	}
+
+	Polynomial::new(poly)
+}
+
+#[test]
+fn addsub() {
+	for _ in 0..1000 {
+		let a = random_poly(50);
+		let b = random_poly(50);
+
+		assert_eq!(a.clone() + b.clone() - b, a);
+	}
+}
+
+#[test]
+fn muldiv() {
+	for _ in 0..1000 {
+		let a = random_poly(20);
+		let b = random_poly(20);
+
+		assert_eq!(a.clone() * b.clone() / b, a);
+	}
+}
+
+#[test]
+fn polydiv() {
+	for _ in 0..1000 {
+		let a = random_poly(20);
+		let b = random_poly(10);
+
+		let (quotient, remainder) = a.clone().div_rem(&b).unwrap();
+
+		assert!(remainder.degree() < b.degree());
+		assert_eq!(quotient * b + remainder, a);
+	}
+}
