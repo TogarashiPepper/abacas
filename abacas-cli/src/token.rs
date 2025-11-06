b282ade96f127c61252e04ad28c06fd9f7d141bd
TogarashiPepper <83902606+TogarashiPepper@users.noreply.github.com>
2025-11-05 16:53:25 -0800

    feat: switch to logos

--- abacas-cli/src/token.rs
+++ abacas-cli/src/token.rs
@@ -1,66 +1,53 @@
 use std::str::FromStr;
 
 use abacas::number::Number;
+use logos::Logos;
 use rug::{Integer, Rational};
 
-#[derive(Debug, Clone, PartialEq, PartialOrd)]
+#[derive(Logos, Debug, Clone, PartialEq, PartialOrd)]
+#[logos(skip r"[ \t\n\f]+")]
 pub enum Token {
+	#[regex(r"-?\d+(\.\d+)?", |lex| parse_num(lex.slice()))]
 	Number(Number),
+	#[regex(r"[a-zA-Z]+", |lex| lex.slice().to_owned())]
 	Ident(String),
 
+	#[token("=")]
 	Eq,
 
+	#[token("+")]
 	Add,
+	#[token("-")]
 	Sub,
+	#[token("*")]
 	Mul,
+	#[token("/")]
 	Div,
+	#[token("^")]
 	Pow,
+	#[token("%")]
 	Rem,
 
+	#[token("(")]
 	LParen,
+	#[token(")")]
 	RParen,
 }
 
-impl Token {
-	pub fn new(token: String) -> Self {
-		match token.as_ref() {
-			"=" => Token::Eq,
+fn parse_num(st: &str) -> Number {
+	let (dec, int) = st.split_once(".").unwrap_or((st, ""));
 
-			"+" => Token::Add,
-			"-" => Token::Sub,
-			"*" => Token::Mul,
-			"/" => Token::Div,
-			"^" => Token::Pow,
-			"%" => Token::Rem,
+	let formatted = if !int.is_empty() {
+		format!("{} / 1{}", dec.to_owned() + int, "0".repeat(int.len()))
+	} else {
+		dec.to_owned()
+	};
 
-			"(" => Token::LParen,
-			")" => Token::RParen,
-			_ => {
-				let split_string = token.split(".").collect::<Vec<&str>>();
-
-				let mut rational_string = String::new();
-
-				if split_string.len() > 1 {
-					let decimal_index = split_string.get(1).map_or("", |v| v).len();
-
-					rational_string = format!(
-						"{} / 1{}",
-						split_string[0].to_owned() + split_string[1],
-						"0".repeat(decimal_index)
-					);
-				}
-
-				let try_rational = Rational::from_str(&rational_string);
-				let try_integer = Integer::from_str(&token);
-
-				if try_integer.is_ok() {
-					Token::Number(Number::from(try_integer.unwrap()))
-				} else if try_rational.is_ok() {
-					Token::Number(Number::from(try_rational.unwrap()))
-				} else {
-					Token::Ident(token)
-				}
-			}
-		}
+	if let Ok(integer) = Integer::from_str(&formatted) {
+		Number::from(integer)
+	} else if let Ok(rational) = Rational::from_str(&formatted) {
+		Number::from(rational)
+	} else {
+		unreachable!()
 	}
 }
