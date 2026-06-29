cd2bb0fa86031b2df52dcf2b559bdf98824e7b40
megatank58 <51410502+megatank58@users.noreply.github.com>
2026-06-29 21:07:53 +0530

    feat(Function): add args

--- abacas-lib/src/function.rs
+++ abacas-lib/src/function.rs
@@ -4,15 +4,17 @@ use crate::expr::{Expr, Symbol};
 /// User defined function
 #[derive(Clone, Debug)]
 pub struct Function {
-	/// Name of the function
-	pub name: Symbol,
+	/// The parameters required by this function as arguments
+	pub args: Vec<Symbol>,
 	/// Expression to execute when this function is called
 	pub execute: Expr,
+	/// Name of the function
+	pub name: Symbol,
 }
 
 impl Function {
 	/// Create a new function
-	pub fn new(name: Symbol, execute: Expr) -> Self {
-		Self { name, execute }
+	pub fn new(args: Vec<Symbol>, execute: Expr, name: Symbol) -> Self {
+		Self { args, execute, name }
 	}
 }
